70be332c8cf4bfd571419c75079fb80d4145ecab
Dan M. Larsen <larsendanmarius@gmail.com>
2024-08-16 18:42:50 +0200

    First iteration implemented

--- js/script.js
+++ js/script.js
@@ -1,7 +1,54 @@
-const slider = document.getElementById('password-length');
+const slider = document.getElementById("password-length");
+const passwordLengthCounter = document.querySelector(
+  ".password-generator__password-length"
+);
+const generateBtnElement = document.getElementById("generate-btn");
+const passwordFormElement = document.getElementById("password-generator__form");
+const passwordOutputElement = document.querySelector(
+  ".password-generator__password"
+);
 
-slider.addEventListener('input', e => {
-    const fillPercent = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+const handleGenerate = function (e) {
+  e.preventDefault();
 
-    slider.style.setProperty('--fill-percent', `${fillPercent}%`);
+  const data = Object.fromEntries(new FormData(e.target));
+  const {
+    "password-length": passwordLength,
+    "include-uppercase": uppercase,
+    "include-lowercase": lowercase,
+    "include-numbers": numbers,
+    "include-symbols": symbols,
+  } = data;
+
+  const allLetters = "abcdefghijklmnopqrstuvwxyz";
+  const allNumbers = "0123456789";
+  const allSymbols = "!-#$%";
+
+  let allCharacters = "";
+
+  if (lowercase) allCharacters += allLetters;
+  if (uppercase) allCharacters += allLetters.toUpperCase();
+  if (numbers) allCharacters += allNumbers;
+  if (symbols) allCharacters += allSymbols;
+
+  let pwdString = "";
+  while (pwdString.length < passwordLength) {
+    const selectedIndex = Math.floor(Math.random() * allCharacters.length);
+    const char = allCharacters[selectedIndex];
+
+    pwdString += char;
+  }
+
+  passwordOutputElement.textContent = pwdString;
+};
+
+slider.addEventListener("input", (e) => {
+  const fillPercent =
+    ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+
+  slider.style.setProperty("--fill-percent", `${fillPercent}%`);
+
+  passwordLengthCounter.textContent = slider.value;
 });
+
+passwordFormElement.addEventListener("submit", handleGenerate);
